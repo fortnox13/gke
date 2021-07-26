72efd45b738b2206be35a1a125538c754e93cc8e
Mykyta_Demchenko <nekitdem@gmail.com>
2021-07-26 09:42:18 +0000

    add monitoring add logging files

--- scripts/bastion_startup_script.sh
+++ scripts/bastion_startup_script.sh
@@ -3,3 +3,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
+curl -sSO https://dl.google.com/cloudagents/install-monitoring-agent.sh
+sudo bash install-monitoring-agent.sh
+curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
+sudo bash install-logging-agent.sh
